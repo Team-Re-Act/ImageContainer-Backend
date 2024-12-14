2d921e1a5bcf424f2fbe7dd15bf46f088789ea58
Tech5818 <pqow5818@gmail.com>
2024-12-14 23:24:45 +0900

    Init: Project Init

---
+++ src/main/kotlin/team/react/imagecontainer/ImageContainerApplication.kt
@@ -0,0 +1,11 @@
+package team.react.imagecontainer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImageContainerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ImageContainerApplication>(*args)
+}
---
+++ src/test/kotlin/team/react/imagecontainer/ImageContainerApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.react.imagecontainer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImageContainerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
